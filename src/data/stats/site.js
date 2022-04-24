22ac8db1fe64bef5902b01c9059d592f0b6c1ba4
Ekbatani <amir.ekbatani@gmail.com>
2022-04-24 14:13:59 +0430

    add anlytics

--- src/data/stats/site.js
+++ src/data/stats/site.js
@@ -2,7 +2,7 @@ import dayjs from 'dayjs';
 
 /* Keys match keys returned by the github api. Fields without keys are
  * mostly jokes. To see everything returned by the github api, run:
- curl https://api.github.com/repos/mldangelo/personal-site
+ curl https://github.com/ekbatani/ekbatani.github.io
  */
 const data = [
   {
